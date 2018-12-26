c6eaf82a9ef343dad74a9de4b25f890551abb661
LayneKM <laynemikan@gmail.com>
2018-12-25 20:32:52 -0500

    Removed local copy of jquery, updated routes

--- routes/careers.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('careers', { title: 'Careers', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js']});
-});
-
-module.exports = router;
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('recipes', { title: 'Recipes', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js', 'javascripts/client-recipes.js']});
+  res.render('recipes', { title: 'Recipes', scripts: [
+    'https://code.jquery.com/jquery-3.3.1.min.js',
+    'javascripts/client-colour.js',
+    'javascripts/client-recipes.js'
+  ]});
 });
 
 module.exports = router;
