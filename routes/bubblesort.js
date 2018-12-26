bda61283555a6381be44208c519304be8b466fe5
LayneKM <laynemikan@gmail.com>
2018-12-25 19:32:50 -0500

    Renamed sortingVisual.js to client-sort.js, updated routes

--- routes/bubblesort.js
+++ routes/bubblesort.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('bubblesort', { title: 'Bubble Sort', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js', 'javascripts/sortingVisual.js']});
+  res.render('bubblesort', { title: 'Bubble Sort', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js', 'javascripts/client-sort.js']});
 });
 
 module.exports = router;
