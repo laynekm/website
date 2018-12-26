f7f807c4312f8e5a58153000e6349d6c6d91cc4a
LayneKM <laynemikan@gmail.com>
2018-12-25 19:27:37 -0500

    Renamed magnetScript.js to client-magnets.js, updated route

--- routes/magnets.js
+++ routes/magnets.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('magnets', { title: 'Magnets', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js','https://cdn.rawgit.com/konvajs/konva/2.1.8/konva.min.js', 'socket.io/socket.io.js', 'javascripts/magnetScript.js']});
+  res.render('magnets', { title: 'Magnets', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js','https://cdn.rawgit.com/konvajs/konva/2.1.8/konva.min.js', 'socket.io/socket.io.js', 'javascripts/client-magnets.js']});
 });
 
 module.exports = router;
