5c5cfca2a3435c9d5221c4514a39bf6478415065
LayneKM <laynemikan@gmail.com>
2018-12-25 19:31:28 -0500

    Renamed snakeGame.js to client-snake.js, updated route

--- routes/snake.js
+++ routes/snake.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('snake', { title: 'Snake', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js', 'javascripts/snakeGame.js']});
+  res.render('snake', { title: 'Snake', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js', 'javascripts/client-snake.js']});
 });
 
 module.exports = router;
