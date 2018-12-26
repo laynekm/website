c7538b593aefe1bf70ac54390a71a7dbf659d3f5
LayneKM <laynemikan@gmail.com>
2018-12-25 19:24:10 -0500

    Renamed colourScript.js to client-colour.js, update routes accordingly

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Homepage', scripts: ['javascripts/colourScript.js', 'javascripts/jquery-1.11.3.js']});
+  res.render('index', { title: 'Homepage', scripts: ['javascripts/client-colour.js', 'javascripts/jquery-1.11.3.js']});
 });
 
 module.exports = router;
