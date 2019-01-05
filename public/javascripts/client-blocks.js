c3b24531ccc411634a0244edac72c4ba0f654ef6
LayneKM <laynemikan@gmail.com>
2019-01-05 15:32:13 -0500

    Refactored piece data structures, added rotation and collission with other pieces

--- public/javascripts/client-blocks.js
+++ public/javascripts/client-blocks.js
@@ -3,12 +3,94 @@ let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
 $(document).keydown(handleKeyDown);
 
+const space = 32;
+const up = 38;
+const down = 40;
+const right = 39;
+const left = 37;
+
+// Colour and relative offsets of game pieces at each rotation state
+// Maybe just save this as a JSON file?
+const pieces = {
+  'I': {
+    colour: '#00ffff',
+    state: [
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 20, y: 40}, {x: 20, y: 60}],
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 40, y: 20}, {x: 60, y: 20}],
+      [{x: 40, y: 00}, {x: 40, y: 20}, {x: 40, y: 40}, {x: 40, y: 60}],
+      [{x: 00, y: 40}, {x: 20, y: 40}, {x: 40, y: 40}, {x: 60, y: 40}],
+    ]
+  },
+  'J': {
+    colour: '#0000ff',
+    state: [
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 20, y: 40}, {x: 00, y: 40}],
+      [{x: 00, y: 00}, {x: 00, y: 20}, {x: 20, y: 20}, {x: 40, y: 20}],
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 20, y: 40}, {x: 40, y: 00}],
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 40, y: 20}, {x: 40, y: 40}],
+    ]
+  },
+  'L': {
+    colour: '#ffa500',
+    state: [
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 20, y: 40}, {x: 40, y: 40}],
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 40, y: 20}, {x: 00, y: 40}],
+      [{x: 00, y: 00}, {x: 20, y: 00}, {x: 20, y: 20}, {x: 20, y: 40}],
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 40, y: 20}, {x: 40, y: 00}],
+    ]
+  },
+  'S': {
+    colour: '#00ff00',
+    state: [
+      [{x: 00, y: 40}, {x: 20, y: 40}, {x: 20, y: 20}, {x: 40, y: 20}],
+      [{x: 00, y: 00}, {x: 00, y: 20}, {x: 20, y: 20}, {x: 20, y: 40}],
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 20, y: 00}, {x: 40, y: 00}],
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 40, y: 20}, {x: 40, y: 40}],
+    ]
+  },
+  'Z': {
+    colour: '#ff0000',
+    state: [
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 20, y: 40}, {x: 40, y: 40}],
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 00, y: 20}, {x: 00, y: 40}],
+      [{x: 00, y: 00}, {x: 20, y: 00}, {x: 20, y: 20}, {x: 40, y: 20}],
+      [{x: 40, y: 00}, {x: 40, y: 20}, {x: 20, y: 20}, {x: 20, y: 40}],
+    ]
+  },
+  'T': {
+    colour: '#aa00ff',
+    state: [
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 40, y: 20}, {x: 20, y: 40}],
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 20, y: 40}, {x: 00, y: 20}],
+      [{x: 00, y: 20}, {x: 20, y: 20}, {x: 40, y: 20}, {x: 20, y: 00}],
+      [{x: 20, y: 00}, {x: 20, y: 20}, {x: 20, y: 40}, {x: 40, y: 20}],
+    ]
+  },
+  'O': {
+    colour: '#ffff00',
+    state: [
+      [{x: 00, y: 00}, {x: 00, y: 20}, {x: 20, y: 00}, {x: 20, y: 20}],
+      [{x: 00, y: 00}, {x: 00, y: 20}, {x: 20, y: 00}, {x: 20, y: 20}],
+      [{x: 00, y: 00}, {x: 00, y: 20}, {x: 20, y: 00}, {x: 20, y: 20}],
+      [{x: 00, y: 00}, {x: 00, y: 20}, {x: 20, y: 00}, {x: 20, y: 20}],
+    ]
+  }
+}
+
+const board = {
+  x: 20,
+  y: 20,
+  h: 400,
+  w: 200,
+  colours: ['#626262', '#4b4b4b']
+}
+
 class Block {
   constructor(x, y, c) {
     this.x = x;
     this.y = y;
-    this.h = 9;
-    this.w = 9;
+    this.h = 20;
+    this.w = 20;
     this.colour = c;
   }
 }
@@ -20,11 +102,11 @@ class Piece {
     this.x = 0;
     this.y = 0;
     this.dy = 0;
-    this.w = 0;
-    this.h = 0;
+    this.r = 0;
   }
 
-  drop() {
+  drop(dy) {
+    this.dy = dy;
     this.y += this.dy;
     for(let block of this.blocks) {
       block.y += this.dy;
@@ -40,153 +122,116 @@ class Piece {
 
   rotate(dr) {
     if(dr > 0){
-
+      this.r < 3 ? this.r += 1 : this.r = 0;
     }
     else{
-
+      this.r > 0 ? this.r -= 1 : this.r = 3;
+    }
+    for(let i in this.blocks) {
+      this.blocks[i].x = this.x + pieces[this.type].state[this.r][i].x;
+      this.blocks[i].y = this.y + pieces[this.type].state[this.r][i].y;
     }
   }
 
   // Populates game piece with new blocks and drops them
-  dropNewBlocks(type, x, y, dy) {
+  // If no argument is provided, it will use a randomly selected one
+  create(type = this.randomType()) {
     this.type = type;
-    this.x = x;
-    this.y = y;
-    this.dy = dy;
-    this.rotation = 0;
+    this.x = 100;
+    this.y = 20;
 
-    if(type === 'I') {
-      this.blocks.push(new Block(x, y, '#00ffff'));
-      this.blocks.push(new Block(x, y + 10, '#00ffff'));
-      this.blocks.push(new Block(x, y + 20, '#00ffff'));
-      this.blocks.push(new Block(x, y + 30, '#00ffff'));
-      this.w = 10;
-      this.h = 40;
+    for(let state of pieces[type].state[0]) {
+      this.blocks.push(new Block(this.x + state.x, this.y + state.y, pieces[type].colour));
     }
-    else if(type === 'J') {
-      this.blocks.push(new Block(x + 10, y, '#0000ff'));
-      this.blocks.push(new Block(x + 10, y + 10, '#0000ff'));
-      this.blocks.push(new Block(x + 10, y + 20, '#0000ff'));
-      this.blocks.push(new Block(x, y + 20, '#0000ff'));
-      this.w = 20;
-      this.h = 30;
-    }
-    else if(type === 'L') {
-      this.blocks.push(new Block(x, y, '#ffa500'));
-      this.blocks.push(new Block(x, y + 10, '#ffa500'));
-      this.blocks.push(new Block(x, y + 20, '#ffa500'));
-      this.blocks.push(new Block(x + 10, y + 20, '#ffa500'));
-      this.w = 20;
-      this.h = 30;
-    }
-    else if(type === 'O') {
-      this.blocks.push(new Block(x, y, '#ffff00'));
-      this.blocks.push(new Block(x + 10, y, '#ffff00'));
-      this.blocks.push(new Block(x, y + 10, '#ffff00'));
-      this.blocks.push(new Block(x + 10, y + 10, '#ffff00'));
-      this.w = 20;
-      this.h = 20;
-    }
-    else if(type === 'S') {
-      this.blocks.push(new Block(x, y + 10, '#00ff00'));
-      this.blocks.push(new Block(x + 10, y, '#00ff00'));
-      this.blocks.push(new Block(x + 10, y + 10, '#00ff00'));
-      this.blocks.push(new Block(x + 20, y, '#00ff00'));
-      this.w = 30;
-      this.h = 20;
-    }
-    else if(type === 'T') {
-      this.blocks.push(new Block(x, y, '#aa00ff'));
-      this.blocks.push(new Block(x + 10, y, '#aa00ff'));
-      this.blocks.push(new Block(x + 20, y, '#aa00ff'));
-      this.blocks.push(new Block(x + 10, y + 10, '#aa00ff'));
-      this.w = 30;
-      this.h = 20;
-    }
-    else if(type === 'Z') {
-      this.blocks.push(new Block(x, y, '#ff0000'));
-      this.blocks.push(new Block(x + 10, y, '#ff0000'));
-      this.blocks.push(new Block(x + 10, y + 10, '#ff0000'));
-      this.blocks.push(new Block(x + 20, y + 10, '#ff0000'));
-      this.w = 30;
-      this.h = 20;
-    }
-    else {
-      // Invalid
-    }
+  }
+
+  // Clears blocks array and returns a copy
+  destroy() {
+    let blockCopies = [...this.blocks];
+    this.blocks = [];
+    return blockCopies;
+  }
+
+  randomType() {
+    let tempPieces = ['I', 'J', 'L', 'S', 'Z', 'T', 'O'];
+    return tempPieces[Math.floor(Math.random() * tempPieces.length)];
   }
 }
 
-let pieces = [];
-let piece1 = new Piece();
-piece1.dropNewBlocks('I', 50, 50, 5);
-pieces.push(piece1);
-
-let piece2 = new Piece();
-piece2.dropNewBlocks('J', 100, 50, 5);
-pieces.push(piece2);
-
-let piece3 = new Piece();
-piece3.dropNewBlocks('L', 150, 50, 5);
-pieces.push(piece3);
-
-let piece4 = new Piece();
-piece4.dropNewBlocks('O', 200, 50, 5);
-pieces.push(piece4);
-
-let piece5 = new Piece();
-piece5.dropNewBlocks('S', 250, 50, 5);
-pieces.push(piece5);
-
-let piece6 = new Piece();
-piece6.dropNewBlocks('T', 300, 50, 5);
-pieces.push(piece6);
-
-let piece7 = new Piece();
-piece7.dropNewBlocks('Z', 350, 50, 5);
-pieces.push(piece7);
-
+let piece = new Piece();
+piece.create('T');
 let blocks = [];
-let level = 1;
 
 $(document).ready(function() {
   timer = setInterval(handleTimer, 100);
 });
 
+let counter = 0;
 function handleTimer() {
   drawCanvas();
-  for(piece of pieces) {
-    if(piece.y + piece.h < canvas.height) {
-      piece.drop();
-    }
-    else {
-      // Add blocks to main block array and remove from game piece
-    }
+  counter++;
+  if(!validDrop(piece, 20)) {
+    blocks.push(...piece.destroy());
+    piece.create();
+  }
+
+  if(counter == 10 && validDrop(piece, 20)) {
+    piece.drop(20);
+    counter = 0;
   }
 }
 
-function handleKeyDown(key){
-  let up = 38;
-  let down = 40;
-  let right = 39;
-  let left = 37;
+function validMove(piece, dx) {
+  for(block of piece.blocks) {
+    if(block.x + dx < board.x ||
+        block.x + block.w + dx > board.x + board.w) {
+        return false;
+    }
+  }
+  return true;
+}
 
-  if(key.which == left || key.which == right | key.which == up || key.which == down){
+function validDrop(piece, dy) {
+  for(let pieceBlock of piece.blocks) {
+    if(pieceBlock.y + pieceBlock.h + dy > board.y + board.h) {
+      return false;
+    }
+    for(let gameBlock of blocks) {
+      if(pieceBlock.y + pieceBlock.h + dy > gameBlock.y && pieceBlock.x === gameBlock.x) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function validRotate(piece, dr) {
+  return true;
+}
+
+function handleKeyDown(key){
+  // Prevents page from scrolling
+  if(key.which == space || key.which == left || key.which == right | key.which == up || key.which == down){
     key.view.event.preventDefault();
   }
 
-  if(key.which == left && piece1.x > 0){
-    piece1.move(-10);
+  if(key.which == left && validMove(piece, -20)){
+    piece.move(-20);
   }
-  else if(key.which == right && piece1.x + piece1.w < canvas.width){
-    piece1.move(10);
+  else if(key.which == right && validMove(piece, 20)){
+    piece.move(20);
   }
 
-  if(key.which == up){
-    piece1.rotate(1);
+  if(key.which == up && validRotate(1)){
+    piece.rotate(1);
   }
-  else if(key.which == down){
-    piece1.rotate(-1);
+  else if(key.which == down && validRotate(-1)){
+    piece.rotate(-1);
+  }
+
+  if(key.which == space && validDrop(piece, 20)){
+    piece.drop(20);
+    counter = 0;
   }
 }
 
@@ -195,16 +240,25 @@ function drawCanvas() {
   context.fillStyle = 'black';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Draw blocks
-  for(let block of blocks) {
-    context.fillStyle = block.colour;
-    context.fillRect(block.x, block.y, block.h, block.w);
+  // Draw board
+  for(let i = 1; i <= 10; i++){
+    i % 2 == 0 ? context.fillStyle = board.colours[0] : context.fillStyle = board.colours[1];
+    context.fillRect(i * 20, 20, 20, board.h);
   }
 
-  for(piece of pieces) {
-    for(let block of piece.blocks) {
-      context.fillStyle = block.colour;
-      context.fillRect(block.x, block.y, block.h, block.w);
-    }
+  // Draw blocks in game board
+  for(let block of blocks) {
+    context.fillStyle = 'black';
+    context.fillRect(block.x, block.y, block.w, block.h)
+    context.fillStyle = block.colour;
+    context.fillRect(block.x + 1, block.y + 1, block.w - 2, block.h - 2);
+  }
+
+  // Draw blocks in game piece
+  for(let block of piece.blocks) {
+    context.fillStyle = 'black';
+    context.fillRect(block.x, block.y, block.w, block.h)
+    context.fillStyle = block.colour;
+    context.fillRect(block.x + 1, block.y + 1, block.w - 2, block.h - 2);
   }
 }
